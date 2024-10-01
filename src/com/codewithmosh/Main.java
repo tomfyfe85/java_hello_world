734a76bb787c3c161df5b2a6c91cc13159dc0db6
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-01 17:33:37 +0100

    types - Primitive vs reference

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,9 +1,12 @@
 package com.codewithmosh;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
