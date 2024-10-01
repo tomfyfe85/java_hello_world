6ea941da6f4e8676ec1afbb90b75aeb820ed4914
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-01 19:46:24 +0100

    types - multi-dimentional arrays

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,12 +1,12 @@
 package com.codewithmosh;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        int[][] numbers = {{1,2,3}, {4,5,6}};
+
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
