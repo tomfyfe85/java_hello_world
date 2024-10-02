b7c87bb052ebd72611a61b7eeddffb504ba29dc5
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-02 15:27:10 +0100

    start of mortage calculator excercise

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,12 +1,27 @@
 package com.codewithmosh;
 
-import java.awt.*;
-import java.util.Arrays;
+//Project - Mortgage calculator
+//when you run program =>
+//Principal: (amount of loan I want to get eg 100000)
+//Annual interest rate: (IE %3.92) = actual interest is .0392 as it was represented as a % -
+//-so what every the user enters, divide by 100 then 12
+//Period (years): (eg 30 years) = multiply by 12 to get number of monthly payments
+//=>
+//Mortgage: $472.81
 
+//first 3 are questions. Mortgage should return your monthly payment
+// wikihow.com/Calculate-Mortage-Payments
+//use Math.pow to raise number power of N where N is no of payments
+
+
+//Principal: (amount of loan I want to get e.g. 100000)
+//Annual interest rate: e.g. %3.45
+//Period (years): e.g. 56
+
+//Mortgage: => money owed each month
 public class Main {
     public static void main(String[] args) {
-        int[][] numbers = {{1,2,3}, {4,5,6}};
-
-        System.out.println(Arrays.deepToString(numbers));
+    // Solution
     }
 }
+
