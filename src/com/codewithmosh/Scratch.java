a914fc284d0211a45e9ad69a13d56be2abc01c54
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-14 21:52:38 +0100

    fizzbuzz in java

--- src/com/codewithmosh/Scratch.java
+++ src/com/codewithmosh/Scratch.java
@@ -1,6 +1,20 @@
 package com.codewithmosh;
 
+import java.util.Scanner;
+
 public class Scratch {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (num % 5 == 0 && num % 3 == 0){
+            System.out.println("fizzbuzz");}
+        else if (num % 5 == 0) {
+            System.out.println("fizz");}
+        else if (num % 3 == 0) {
+            System.out.println("buzz");}
+        else
+            System.out.println(num);
     }
 }
+
