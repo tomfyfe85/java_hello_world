26a27ddefe43e0c60ddd4f870377d211bbacd325
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-15 23:00:11 +0100

    up to mortage calculater pt 2

--- src/com/codewithmosh/Scratch.java
+++ src/com/codewithmosh/Scratch.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Scratch {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+       String[] fruits = { "apple", "mango", "orange" };
 
-        if (num % 5 == 0 && num % 3 == 0){
-            System.out.println("fizzbuzz");}
-        else if (num % 5 == 0) {
-            System.out.println("fizz");}
-        else if (num % 3 == 0) {
-            System.out.println("buzz");}
-        else
-            System.out.println(num);
+       for (int i = 0; i < fruits.length; i++)
+           System.out.println(fruits[i]);
+
+       for (String fruit : fruits)
+           System.out.println(fruit);
     }
 }
 
