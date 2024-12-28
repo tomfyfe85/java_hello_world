b2ef3674d6ace10e8dc2cd381631b310aea26da7
Tom FYFE <tomfyfe85@gmail.com>
2024-12-28 09:45:19 +0000

    strings Completed

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,13 +1,10 @@
 package com.codewithmosh;
 
-//import java.util.Scanner;
-
 import java.awt.*;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-    Point point1 = new Point(1, 1);
-    Point point2 = point1;
+    String message = "Hello World" + "!!";
+    System.out.println(message.replace("e", "2"));
     }
 }
