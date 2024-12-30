82292c6d59ab233e42c1a6e397cf67b41bab32e4
Tom FYFE <tomfyfe85@gmail.com>
2024-12-30 09:26:00 +0000

    multidimensional arrays complete

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2,3,5,1,4};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));    }
+    int[][] numbers = {{1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
+        }
 }
