7460f752578bcb2ef410d4fb213563ca333d184b
Tom FYFE <tomfyfe85@gmail.com>
2025-01-07 22:59:35 +0000

    formatting numbers

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,9 +1,10 @@
 package com.codewithmosh;
 
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-        int result = (int)Math.round(Math.random() * 100);
-        System.out.println(result);
+        NumberFormat
     }
 }
