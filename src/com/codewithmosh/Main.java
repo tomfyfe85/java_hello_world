dd6b460afa33bb58822dca4f7168ef39f64eb331
Tom FYFE <tomfyfe85@gmail.com>
2025-01-07 23:40:17 +0000

    formatting numbers complete

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -5,6 +5,7 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        NumberFormat
+        String percentage = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(percentage);
     }
 }
