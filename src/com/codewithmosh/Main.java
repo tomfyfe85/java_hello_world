d724f773fedde3c8b3a03bae22ac5744fa9140e0
Tom FYFE <tomfyfe85@gmail.com>
2025-01-09 23:58:43 +0000

    beginning mortage calculator

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -6,11 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String name = scanner.nextLine().trim();
-        System.out.println("you are " + name);
-
 
     }
 }
