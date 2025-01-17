aac5d48783a66a6988c7da7a61479c77f06df096
tomfyfe <tomfyfe85@gmail.com>
2025-01-17 10:45:42 +0000

    Ternary operator complete

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,11 +1,10 @@
 package com.codewithmosh;
 
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy";
+        System.out.println(className);
     }
 }
