e1188e5f22bb699f934aafea6a7379c8951a4fac
tomfyfe <tomfyfe85@gmail.com>
2025-03-26 12:23:42 +0000

    completed encapsulation

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -2,12 +2,11 @@ package com.codewithmosh;
 
 public class Main {
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text);
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate =20;
+        int wage = employee.calculateWage(10);
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box2");
-        System.out.println(textBox2.text);
+        System.out.println(wage);
     }
 }
