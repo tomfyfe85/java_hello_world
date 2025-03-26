132af044559cc7f989174e60a694f94bb8c225b6
tomfyfe <tomfyfe85@gmail.com>
2025-03-26 11:00:37 +0000

    creating objects complete

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -2,14 +2,12 @@ package com.codewithmosh;
 
 public class Main {
     public static void main(String[] args) {
-       String name1 =  greetUser("Tom", "Fyfe");
-       String name2 =  greetUser("John", "Spartan");
-        System.out.println(name1);
-        System.out.println(name2);
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
 
-    }
-
-    public static String greetUser(String firstName, String lastName){
-        return "Hello " + firstName + " " + lastName;
+        var textBox2 = new TextBox();
+        textBox2.setText("Box2");
+        System.out.println(textBox2.text);
     }
 }
