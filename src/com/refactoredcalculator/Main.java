73c9151af5ccc7f8b246dd985b2d1ffd2eb7036e
tomfyfe <tomfyfe85@gmail.com>
2025-04-15 15:09:31 +0100

    moving stqtif fields completed

--- src/com/refactoredcalculator/Main.java
+++ src/com/refactoredcalculator/Main.java
@@ -1,16 +1,15 @@
 package com.refactoredcalculator;
 
 public class Main {
-    final static byte MONTHS_IN_YEAR = 12;
-    final static byte PERCENT = 100;
 
     public static void main(String[] args) {
         int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
         float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        MortageReport.printMortgage(principal, annualInterest, years);
-        MortageReport.printPaymentSchedule(principal, annualInterest, years);
+        var calculator = new MortageCalculator(principal, annualInterest, years);
+        var report = new MortageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
     }
-
 }
