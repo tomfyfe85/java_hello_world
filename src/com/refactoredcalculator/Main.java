fc2f471307a15e39d9b59a7bb276c6e885c93b00
tomfyfe <tomfyfe85@gmail.com>
2025-05-07 21:08:55 +0100

    update

--- src/com/refactoredcalculator/Main.java
+++ src/com/refactoredcalculator/Main.java
@@ -7,8 +7,8 @@ public class Main {
         float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        var calculator = new MortageCalculator(principal, annualInterest, years);
-        var report = new MortageReport(calculator);
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        var report = new MortgageReport(calculator);
         report.printMortgage();
         report.printPaymentSchedule();
     }
--- src/com/refactoredcalculator/MortgageReport.java
+++ src/com/refactoredcalculator/MortgageReport.java
@@ -2,17 +2,20 @@ package com.refactoredcalculator;
 
 import java.text.NumberFormat;
 
-public class MortageReport {
+public class MortgageReport {
 
-    private MortageCalculator calculator;
+    private final NumberFormat currency;
+    private MortgageCalculator calculator;
 
-    public MortageReport(MortageCalculator calculator) {
+    public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
+
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
@@ -23,9 +26,9 @@ public class MortageReport {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
-        for (short month = 1; month <= calculator.getYears() * MortageCalculator.MONTHS_IN_YEAR; month++) {
-            double balance = calculator.calculateBalance(month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
         }
     }
-}
+
+
