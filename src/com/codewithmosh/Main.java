e0cf4d97b067017dac39668f7642836bdec8c804
tomfyfe <tomfyfe85@gmail.com>
2025-05-28 23:25:04 +0100

    comparing objects

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,18 +4,10 @@ import org.w3c.dom.Text;
 
 public class Main {
     public static void main(String[] args) {
-    var control = new UIControl(true);
-    var textBox = new TextBox();
-    show(textBox);
-    }
-
-    public static void show(UIControl control){
-        if (control instanceof TextBox) {
-            var textBox = (TextBox)control;
-            textBox.setText("Hello World");
-        }
-
-        System.out.println(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
 
